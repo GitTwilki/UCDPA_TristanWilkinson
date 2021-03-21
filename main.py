12a97f62c076c048fab2c59b1f2470bbfa2eb92f
Tristan <twilki@gmail.com>
2021-03-21 17:41:38 +0000

    Final refactoring of code

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ def load_from_db():
 # param: a stock id
 # return: subset of the columns we want
 def call_yf_api(p_stock):
-  gme_st = yf.Ticker(p_stock)
-  #gme_st_dr = gme_st.history(start="2021-02-01", end="2021-02-28")
-  gme_st_dr = gme_st.history(start=st_date, end=end_date)
-  return gme_st_dr[['Open', 'High', 'Low', 'Close']]
+    gme_st = yf.Ticker(p_stock)
+    # gme_st_dr = gme_st.history(start="2021-02-01", end="2021-02-28")
+    gme_st_dr = gme_st.history(start=st_date, end=end_date)
+    return gme_st_dr[['Open', 'High', 'Low', 'Close']]
 
 
 # Function to format the sentiment data
@@ -50,36 +50,36 @@ def call_yf_api(p_stock):
 # return: formatted data
 def format_data(p_wsb_data):
     wsb_data_sub = p_wsb_data.loc[:, ['title', 'comms_num', 'body', 'timestamp']]
-    ## add a formatted date column so we can match indexes - wsb_data_formatted = datetime.datetime.strptime(when, '%Y-%m-%d').date()
-    ## convert column to datetime
+    # add a formatted date column so we can match indexes
+    # convert column to datetime
     wsb_data_sub['timestamp'] = pd.to_datetime(wsb_data_sub['timestamp'])
-    ## add just the date column
+    # add just the date column
     wsb_data_sub["DATE"] = wsb_data_sub['timestamp'].dt.date
-    ## set the new column as the index
+    # set the new column as the index
     wsb_data_inx = wsb_data_sub.set_index("DATE").sort_index()
     # subset only date range of data
     # startdate = pd.to_datetime("2021-01-28").date()  ## Dates for our analysis
     # enddate = pd.to_datetime("2021-02-20").date()
     startdate = pd.to_datetime(st_date).date()
     enddate = pd.to_datetime(end_date).date()
-    #return the formatted data
+    # return the formatted data
     return wsb_data_inx.loc[startdate:enddate]
 
 
 # Function to aggregate the sentiment data
 # param: formatted data
 # return: aggregated data
-def aggregate_data(p_wsb_data_final,p_stock_data):
+def aggregate_data(p_wsb_data_final, p_stock_data):
     # pandas series of the aggregate counts
     wsb_agg_data = p_wsb_data_final.groupby(wsb_data_final.index)['title'].count()
     # convert series to DF to merge with stock prices
     wsb_agg_data_df = pd.DataFrame(wsb_agg_data)
     wsb_agg_data_df.rename(columns={'title': 'Num_Posts'}, inplace=True)
-    ## merge the post count data with stock price join on the index (date in both cases YYYY-MM-DD)
-    mergedDF = pd.merge(wsb_agg_data_df, p_stock_data, how='outer', left_index=True, right_index=True)
-    ## fill the blank weekend market data with last close values
-    mergedDF['Close'] = mergedDF['Close'].ffill()
-    return mergedDF.bfill(axis=1)
+    # merge the post count data with stock price join on the index (date in both cases YYYY-MM-DD)
+    mergeddf = pd.merge(wsb_agg_data_df, p_stock_data, how='outer', left_index=True, right_index=True)
+    # fill the blank weekend market data with last close values
+    mergeddf['Close'] = mergeddf['Close'].ffill()
+    return mergeddf.bfill(axis=1)
 
 
 # PROG MAIN SECTION
@@ -91,7 +91,7 @@ wsb_data = load_panda_csv(fname)
 if db_api == 'A':
     # load the stock data from yfinance API
     stock_data = call_yf_api(sname)
-elif db_api == 'D':
+else:
     # load the data from the DB
     stock_data = load_from_db()
 
@@ -99,7 +99,7 @@ elif db_api == 'D':
 wsb_data_final = format_data(wsb_data)
 
 # call the aggregate_data function to merge and agg data
-mergedDF = aggregate_data(wsb_data_final,stock_data)
+mergedDF = aggregate_data(wsb_data_final, stock_data)
 
 print(mergedDF)
 
--- visMod.py
+++ visMod.py
@@ -13,7 +13,7 @@ def show_num_posts(p_data):
     fig, ax = plt.subplots()
     ax.bar(p_data.index,p_data["Num_Posts"],color="black",alpha=.75)
     plt.title("Number of forum posts(Day)",fontsize = 18, alpha = .75)
-    plt.xlabel("Day of the Month 28 Jan - 20 Feb 2021")
+    plt.xlabel("Date")
     plt.ylabel("Number of Posts")
     ax.xaxis.set_tick_params(reset=True)
     ax.xaxis.set_major_locator(mdates.DayLocator())
@@ -32,9 +32,9 @@ def show_high_low_plot(p_data):
     fig, ax = plt.subplots()
     ax.plot(p_data["High"], 'black', label='High',alpha=.75)
     ax.plot(p_data["Low"], 'red', label='Low',alpha=.75)
-    plt.title("High/Low Stock Price ($)", fontsize=18, alpha=.75)
+    plt.title("High/Low Share Price ($)", fontsize=18, alpha=.75)
     plt.xlabel("Date")
-    plt.ylabel("Stock Price")
+    plt.ylabel("Share Price")
     leg = ax.legend()
     ax.xaxis.set_tick_params(reset=True)
     ax.xaxis.set_major_locator(mdates.DayLocator())
@@ -54,7 +54,7 @@ def show_candlestick(p_data):
         edge='black',
         wick={'up': 'g', 'down': 'r'})
     s = mpf.make_mpf_style(base_mpl_style="ggplot", marketcolors=mc)
-    fig, ax = mpf.plot(p_data, type = 'candle',title='Candlestick chart for GME',ylabel = 'Stock Price ($)',returnfig = True,style=s)
+    fig, ax = mpf.plot(p_data, type = 'candle',title='Candlestick chart for GME',ylabel = 'Share Price ($)',returnfig = True,style=s)
     plt.show()
 
 
@@ -71,7 +71,7 @@ def show_dual_axis_corr(p_data):
     ax1.plot(left_data, color="red",alpha=.75)
     ax1.tick_params(axis='y', labelcolor="red")
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    ax2.set_ylabel('Stock Price ($)', color="black")  # we already handled the x-label with ax1
+    ax2.set_ylabel('Share Price ($)', color="black")  # we already handled the x-label with ax1
     ax2.plot(right_data, color="black",alpha=.75)
     ax2.tick_params(axis='y', labelcolor="black")
     plt.title("Number of Reddit Posts/Day/Share Price", fontsize=18, alpha=.75)
@@ -118,7 +118,7 @@ def show_scatter(p_data):
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.scatter(x=p_data["Num_Posts"], y=p_data["High"],color='red',alpha=.75)
     plt.xlabel("Number of Posts")
-    plt.ylabel("Stock Price ($)")
+    plt.ylabel("Share Price ($)")
     plt.title("Price/Post Correlation", fontsize=18, alpha=.75)
     plt.locator_params(axis='y', nbins=10)
     plt.locator_params(axis='x', nbins=20)
